07d87046947962d0ff7752ceb912522c039dc842
masudrana5 <abdullainfo30@gmail.com>
2022-11-23 22:09:26 +0600

    second hand products section is added done

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import AdvertisePage from '../AdvertisePage/AdvertisePage';
 import Banner from '../Banner/Banner';
+import SecondHandProducts from '../SecondHandProducts/SecondHandProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AdvertisePage></AdvertisePage>
+            <SecondHandProducts></SecondHandProducts>
         </div>
     );
 };
---
+++ src/components/Home/SecondHandProducts/SecondHandProducts.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+
+const SecondHandProducts = () => {
+    return (
+        <section className='mx-auto max-w-screen-xl px-4 py-8'>
+            <div>
+                <div className='flex flex-col lg:flex-row justify-between bg-slate-200 p-5'>
+                    <button className='btn btn-ghost'>Oppo</button>
+                    <button className='btn btn-ghost'>Vivo</button>
+                    <button className='btn btn-ghost'>Realme</button>
+                    <button className='btn btn-ghost'>Samsung</button>
+                    <button className='btn btn-ghost'>Apple</button>
+                </div>
+            </div>
+            <div>
+                <section>
+                    <div class="mx-auto max-w-screen-xl px-4 py-8">
+                        <div>
+                            <span class="inline-block h-1 w-12 bg-red-700"></span>
+
+                            <h2
+                                class="mt-1 text-2xl font-extrabold uppercase tracking-wide lg:text-3xl"
+                            >
+                                Trainers
+                            </h2>
+                        </div>
+
+                        <div class="mt-8 grid grid-cols-2 gap-x-4 gap-y-8 lg:grid-cols-4">
+                            <a href="#" class="block">
+                                <div class="flex justify-center">
+                                    <strong
+                                        class="relative h-6 bg-black px-4 text-xs uppercase leading-6 text-white"
+                                    >
+                                        New
+                                    </strong>
+                                </div>
+
+                                <img
+                                    alt="Trainer"
+                                    src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                                    class="-mt-3 h-96 w-full object-cover"
+                                />
+
+                                <h3 class="mt-4 text-sm text-black/90">
+                                    Limited Edition Sports Trainer
+                                </h3>
+
+                                <div class="mt-4 flex items-center justify-between font-bold">
+                                    <p class="text-lg">$189.99</p>
+
+                                    <p class="text-xs uppercase tracking-wide">6 Colors</p>
+                                </div>
+                            </a>
+
+                            <a href="#" class="block">
+                                <div class="flex justify-center">
+                                    <strong
+                                        class="relative h-6 bg-black px-4 text-xs uppercase leading-6 text-white"
+                                    >
+                                        New
+                                    </strong>
+                                </div>
+
+                                <img
+                                    alt="Trainer"
+                                    src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                                    class="-mt-3 h-96 w-full object-cover"
+                                />
+
+                                <h3 class="mt-4 text-sm text-black/90">
+                                    Limited Edition Sports Trainer
+                                </h3>
+
+                                <div class="mt-4 flex items-center justify-between font-bold">
+                                    <p class="text-lg">$189.99</p>
+
+                                    <p class="text-xs uppercase tracking-wide">6 Colors</p>
+                                </div>
+                            </a>
+
+                            <a href="#" class="block">
+                                <div class="flex justify-center">
+                                    <strong
+                                        class="relative h-6 bg-black px-4 text-xs uppercase leading-6 text-white"
+                                    >
+                                        New
+                                    </strong>
+                                </div>
+
+                                <img
+                                    alt="Trainer"
+                                    src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                                    class="-mt-3 h-96 w-full object-cover"
+                                />
+
+                                <h3 class="mt-4 text-sm text-black/90">
+                                    Limited Edition Sports Trainer
+                                </h3>
+
+                                <div class="mt-4 flex items-center justify-between font-bold">
+                                    <p class="text-lg">$189.99</p>
+
+                                    <p class="text-xs uppercase tracking-wide">6 Colors</p>
+                                </div>
+                            </a>
+
+                            <a href="#" class="block">
+                                <div class="flex justify-center">
+                                    <strong
+                                        class="relative h-6 bg-black px-4 text-xs uppercase leading-6 text-white"
+                                    >
+                                        New
+                                    </strong>
+                                </div>
+
+                                <img
+                                    alt="Trainer"
+                                    src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                                    class="-mt-3 h-96 w-full object-cover"
+                                />
+
+                                <h3 class="mt-4 text-sm text-black/90">
+                                    Limited Edition Sports Trainer
+                                </h3>
+
+                                <div class="mt-4 flex items-center justify-between font-bold">
+                                    <p class="text-lg">$189.99</p>
+
+                                    <p class="text-xs uppercase tracking-wide">6 Colors</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+
+        </section>
+    );
+};
+
+export default SecondHandProducts;
